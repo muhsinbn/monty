bd9fae0e058e21ea238b387f323cc18efc415160
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-02-02 15:20:03 -0800

    The opcode pint prints the value at the top of the stack, followed by a new line.

--- task_1.c
+++ task_1.c
@@ -1,72 +1,20 @@
 #include "monty.h"
 
 /**
- * myPush - pushes an element into the stack
- * @start: A pointer to the head of stack
- * @iterator: line where opcodes appears
- */
-
-void myPush(stack_t **start, unsigned int iterator)
-{
-	char *arguments = mont.av;
-	stack_t *new;
-	int val;
-
-	if (!arguments)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", iterator);
-		fclose(mont.myfile);
-		free(mont.subjects);
-		free_allstacks(*start);
-		exit(EXIT_FAILURE);
-	}
-	val = atoi(arguments);
-	new = malloc(sizeof(stack_t));
-
-	if (!new)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		fclose(mont.myfile);
-		free(mont.subjects);
-		free_allstacks(*start);
-		exit(EXIT_FAILURE);
-	}
-	new->n = val;
-	new->prev = NULL;
-
-	if (*start)
-	{
-		new->next = *start;
-		(*start)->prev = new;
-	}
-	else
-	{
-		new->next = NULL;
-	}
-	*start = new;
-}
-
-
-#include "monty.h"
-
-/**
- * myPall - prints the stack
+ * myPint - prints the top of the stack
  * @start: head of the stack
- * @iterator: counter not  used
- * Return: nothing to return
+ * @iterator: line number count
+ *
  */
-void myPall(stack_t **start, unsigned int iterator)
+void myPint(stack_t **start, unsigned int iterator)
 {
-	stack_t *st;
-	(void)iterator;
-
-	st = *start;
-	if (st == NULL)
-		return;
-
-	while (st)
+	if (*start == NULL)
 	{
-		printf("%d\n", st->n);
-		st = st->next;
+		fprintf(stderr, "L%u: can't pint, stack empty\n", iterator);
+		fclose(mont.myfile);
+		free(mont.subjects);
+		free_allstacks(*start);
+		exit(EXIT_FAILURE);
 	}
+	printf("%d\n", (*start)->n);
 }
